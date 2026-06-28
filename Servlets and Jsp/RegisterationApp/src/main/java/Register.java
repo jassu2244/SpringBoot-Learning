61e054c8e107e4349fbf3ecbb230904bdb27b807
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-06-28 06:29:10 +0530

    Added maven folders

--- Servlets and Jsp/RegisterationApp/src/main/java/Register.java
+++ Servlets and Jsp/RegisterationApp/src/main/java/Register.java
@@ -1,10 +1,4 @@
 
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;

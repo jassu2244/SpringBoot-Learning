e3ce6ff1673fcc927ffde245061759817209cb05
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-07-09 09:54:52 +0530

    created alien object and did autowiring

--- Spring1/src/main/java/com/telusko/App2.java
+++ Spring1/src/main/java/com/telusko/App2.java
@@ -10,10 +10,18 @@ public class App2 {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 //        Desktop dt = context.getBean("beast", Desktop.class);
-        Desktop dt = context.getBean(Desktop.class);
-        dt.compile();
 
-        Desktop dt1 = context.getBean(Desktop.class);
-        dt.compile();
+        //after this we need to create bean of this class in AppConfig
+        Alien obj1 = context.getBean(Alien.class);
+        System.out.println(obj1.getAge());
+        obj1.code(); //right now when we run this it will create an object of alien but gives an error because it has 2 things and
+        //abhi we just defined age, we did nothing for com(computer object), so let's set this too using setCom()
+
+
+//        Desktop dt = context.getBean(Desktop.class);
+//        dt.compile();
+//
+//        Desktop dt1 = context.getBean(Desktop.class);
+//        dt.compile();
     }
 }
--- Spring1/src/main/java/com/telusko/config/AppConfig.java
+++ Spring1/src/main/java/com/telusko/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.telusko.config;
 
+import com.telusko.Alien;
+import com.telusko.Computer;
 import com.telusko.Desktop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -9,13 +12,23 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class AppConfig {
 
+    @Bean
+    public Alien alien(@Autowired Computer com) { // using this autowired is optional, it will still work in newer version
+        Alien obj = new Alien();
+        obj.setAge(25);
+        obj.setCom(com);
+        //obj.setCom(desktop()); // this will remove the error, but it is tightly coupled right now, what if we have laptop, so in
+        //constructor just pass ((Computer com) a computer object)
+        return obj;
+    }
+
     // this is executed by spring, this Bean annotation tells spring to create a bean like we are creating manually in XML
     //name of your bean is by default the name that is of your method means like here it is desktop.
     //also we can specify name using this tag too and give multiple words
 //    @Bean(name = {"com2", "desktop1", "beast"})
 
     @Bean
-    @Scope("prototype") //this annotation is used to define scope like giving prototype rather than singleton
+    //@Scope("prototype") //this annotation is used to define scope like giving prototype rather than singleton
     // so when we give prototype even for same class it will create multiple object rather than singleton where it uses same object
     //to reference it even though we create multiple references
     public Desktop desktop(){

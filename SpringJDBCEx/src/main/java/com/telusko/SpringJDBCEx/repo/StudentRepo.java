227802754e6efc3e65bba356383e34f20de9637a
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-07-09 21:21:32 +0530

    used RowMapper to map data to db

--- SpringJDBCEx/src/main/java/com/telusko/SpringJDBCEx/repo/StudentRepo.java
+++ SpringJDBCEx/src/main/java/com/telusko/SpringJDBCEx/repo/StudentRepo.java
@@ -3,8 +3,11 @@ package com.telusko.SpringJDBCEx.repo;
 import com.telusko.SpringJDBCEx.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +35,39 @@ public class StudentRepo {
 
     public List<Student> findAll() {
 
-        List<Student> students = new ArrayList<>();
-        return students;
+        String sql = "select * from student";
+
+//        RowMapper<Student> mapper= new RowMapper<Student>() {
+//            @Override
+//            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+//
+//                Student s = new Student();
+//                s.setRollNo(rs.getInt("rollno")); //here you have to specify column name
+//                s.setName(rs.getString("name"));
+//                s.setMarks(rs.getInt("marks"));
+//
+//                return s;
+//            }
+//        };
+
+        //Doing this using lambda expression
+//        RowMapper<Student> mapper = (rs, rowNum) -> {
+//            Student s = new Student();
+//            s.setRollNo(rs.getInt("rollno")); //here you have to specify column name
+//            s.setName(rs.getString("name"));
+//            s.setMarks(rs.getInt("marks"));
+//            return s;
+//        };
+
+        return jdbc.query(sql, (rs, rowNum) -> {
+            Student s = new Student();
+            s.setRollNo(rs.getInt("rollno")); //here you have to specify column name
+            s.setName(rs.getString("name"));
+            s.setMarks(rs.getInt("marks"));
+            return s;
+        });
+
+
+//        return jdbc.query(sql, mapper);
     }
 }

f86601211f73b2a9c454e29134f4715efd50595e
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-07-10 21:54:10 +0530

    completed job app with mvc design

---
+++ JobApp/src/main/java/com/telusko/JobApp/controller/JobController.java
@@ -0,0 +1,50 @@
+package com.telusko.JobApp.controller;
+
+import com.telusko.JobApp.model.JobPost;
+import com.telusko.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+//for get methods use GetMapping and for post method use PostMapping, rather than RequestMapping or you can specify in
+//request mapping too what kind of request is this @RequestMapping("/", post)
+
+@Controller
+public class JobController {
+
+    @Autowired
+    private JobService service;
+
+
+    @GetMapping({"/", "home"})
+    public String home() {
+        return "home";
+    }
+
+
+    @GetMapping("addjob")
+    public String addJob() {
+        return "addjob";
+    }
+
+
+    @PostMapping("handleForm")
+    public String handleForm(JobPost jobPost) {
+        service.addJob(jobPost);
+        return "success";
+
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model m) {
+        List<JobPost> jobs = service.getAllJobs();
+        m.addAttribute("jobPosts", jobs);
+
+        return "viewalljobs";
+    }
+
+}
---
+++ JobApp/src/main/java/com/telusko/JobApp/model/JobPost.java
@@ -0,0 +1,21 @@
+package com.telusko.JobApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+//the above 3 annotation are from lombok that helps with getters and setters so we do not need to create them manually
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component //this is to make this spring component
+public class JobPost {
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private Integer reqExperience;
+    private List<String> postTechStack;
+}

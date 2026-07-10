17d8ef2c74bf3b58ce26c72a3e4cb8240f7e0d3c
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-07-10 11:10:04 +0530

    used model and view instead of model, and set application properties so that don't have to pass the extension in method

--- SpringBootWeb-1/src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ SpringBootWeb-1/src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -3,7 +3,10 @@ package com.telusko.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller //this tells spring that this is servlet controller
 public class HomeController {
@@ -12,20 +15,33 @@ public class HomeController {
     //so we need tomcat jasper dependency to show jsp page in the browser
     public String home() {
         System.out.println("home method called");
-        return "index.jsp"; //this will not work right now because browser don't know which method to call on which
+        //return "index.jsp"; //this will not work right now because browser don't know which method to call on which
         //request, So we need to first do Mapping
+        return "index"; //to pass it like this without extension we can use spring application properties(prefix and suffix)
     }
 
     @RequestMapping("add") //here we need to pass the url where we want to map
-    public String add(HttpServletRequest req, HttpSession session) {
+    //public String add(int num1, int num2, HttpSession session) { //when you pass the query parameter here with same name
+        //it matches the variable name and pass the parameter to method
 
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+        //used RequestParam annotation to change variable name and make code more simple
+//    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session) {
+
+//    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv) {
+
+//        int num1 = Integer.parseInt(req.getParameter("num1"));
+//        int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
 
-        session.setAttribute("result", result); // here we pass name and result
+//        session.setAttribute("result", result); // here we pass name and result
+        mv.addObject("result", result); //using ModelAndView in place of Model to do the same
+        mv.setViewName("result"); //we need to pass this to make it find this result.jsp page
 
-        return "result.jsp";
+//        return "result.jsp";
+//        return "result";
 
+        return mv; //using model and view we don't need to pass the result page like this
     }
 }

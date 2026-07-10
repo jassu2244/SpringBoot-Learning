c31510af8c311c7fc34d5e1d24d332ee2632944f
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-07-10 09:57:17 +0530

    sent response to the web browser

--- ServletEx/src/main/java/com/telusko/HelloServlet.java
+++ ServletEx/src/main/java/com/telusko/HelloServlet.java
@@ -4,11 +4,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 //@WebServlet("/hello") // this is the way when using external tomcat and not embedded
 public class HelloServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res) {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In service");
+
+        res.setContentType("text/html"); // this line tells that we are sending html and not just simple text
+
+        PrintWriter out = res.getWriter(); //creating an object to send output to web page
+        out.println("<h2><b>Hello World</b></h2>");
     }
 }

51df55edd1a41e070a2b47d2142153adad59a411
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-07-11 09:46:20 +0530

    created controller, service, repo and model layers and getting data from the server

---
+++ spring-boot-rest/src/main/java/com/telusko/spring_boot_rest/JobRestController.java
@@ -0,0 +1,31 @@
+package com.telusko.spring_boot_rest;
+
+import com.telusko.spring_boot_rest.model.JobPost;
+import com.telusko.spring_boot_rest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:3000") // we are passing frontend url here in this annotation to remove
+//cross-origin error and in frontend we just need to pass url of backend that is localhost:8080/jobPosts
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts") //also here whatever I pass will become that after "/jobPostss" thing
+   // @ResponseBody //by default when we use Controller it searches for view page so we need this annotation to tell browser that
+    //we are searching for data and not view page, If we use Rest Controller, we don't need response body annotation
+    public List<JobPost> getAllJobs() {
+        return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJob(@PathVariable("postId") int postId) { //this annotation tells what variable to search in query string
+        return service.getJob(postId);
+    }
+}

42a46bd64db37128ed8a2046093f6e0e500cd965
jassu2244 <jasmeetsinghchhabra55@gmail.com>
2026-07-15 22:45:54 +0530

    implemented Aspect Oriented Programming

--- spring-data-rest-demo/src/main/java/com/telusko/spring_data_rest_demo/repo/JobRepo.java
+++ spring-data-rest-demo/src/main/java/com/telusko/spring_data_rest_demo/repo/JobRepo.java
@@ -4,8 +4,10 @@ import com.telusko.spring_data_rest_demo.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface JobRepo extends JpaRepository<JobPost, Integer> {
-    //List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
 }
